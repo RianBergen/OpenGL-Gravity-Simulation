8d7bc12009e25f339720247ad340bb8cbdec7e35
Rian Bergen <rianb98@gmail.com>
2022-02-24 16:32:13 -0500

    Added Model

--- OpenGL-Gravity-Simulation/Maths/Shapes/Shape.cs
+++ OpenGL-Gravity-Simulation/Maths/Shapes/Shape.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 
 namespace OpenGL_Gravity_Simulation.Maths.Shapes
 {
+    /// <summary>
+    /// Class to Define Neccessary Shape Functions
+    /// </summary>
     class Shape
     {
+        /// <summary>
+        /// List of Vertices for OpenGL
+        /// </summary>
         private List<float> _Vertices;
+
+        /// <summary>
+        /// List of Vertices for OpenGL
+        /// </summary>
         public List<float> Vertices { get { return _Vertices; } set { _Vertices = value; } }
 
+        
+
+        /// <summary>
+        /// List of Indices for OpenGL
+        /// </summary>
         private List<uint> _Indices;
+
+        /// <summary>
+        /// List of Indices for OpenGL
+        /// </summary>
         public List<uint> Indices { get { return _Indices; } set { _Indices = value; } }
     }
 }
